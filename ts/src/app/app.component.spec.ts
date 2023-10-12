28ce1eec005ecd7d357f617c4ea6e4d0bc2ad5af
Helter Xavier <helter.xavier@hotmail.com>
2023-10-12 03:27:23 -0300

    1210

--- ts/src/app/app.component.spec.ts
+++ ts/src/app/app.component.spec.ts
@@ -1,31 +1,46 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { Component } from '@angular/core';
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
 
-  it(`should have as title 'ts'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ts');
-  });
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'DevChuva';
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ts app is running!');
-  });
-});
+
+  visibleParagraph: string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In ipsum doloribus natus! Consectetur distinctio, ducimus tenetur dolorem sunt voluptas enim doloremque, voluptatibus obcaecati, dicta alias voluptatum expedita delectus. Illum, consectetur! Lorem ipsum dolor sit amet consectetur adipisicing elit. In ipsum doloribus natus! Consectetur distinctio, ducimus tenetur dolorem sunt voluptas enim doloremque, voluptatibus obcaecati, dicta alias voluptatum expedita delectus. Illum, consectetur! Lorem ipsum dolor sit amet consectetur adipisicing elit. In ipsum doloribus natus! Consectetur distinctio, ducimus tenetur dolorem sunt voluptas enim sum dolor sit amet consectetur...';
+  hiddenParagraphs: string[] = [
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. In ipsum doloribus natus! Consectetur distinctio, ducimus tenetur dolorem sunt voluptas enim doloremque, voluptatibus obcaecati, dicta alias voluptatum expedita delectus. Illum, consectetur! Lorem ipsum dolor sit amet consectetur adipisicing elit. In ipsum doloribus natus! Consectetur distinctio, ducimus tenetur dolorem sunt voluptas enim doloremque, voluptatibus obcaecati, dicta alias voluptatum expedita delectus. Illum, consectetur! Lorem ipsum dolor sit amet consectetur adipisicing elit. In ipsum doloribus natus! Consectetur distinctio, ducimus tenetur dolorem sunt voluptas enim',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. In ipsum doloribus natus! Consectetur distinctio, ducimus tenetur dolorem sunt voluptas enim doloremque, voluptatibus obcaecati, dicta alias voluptatum expedita delectus. Illum, consectetur! Lorem ipsum dolor sit amet consectetur adipisicing elit. In ipsum doloribus natus! Consectetur distinctio, ducimus tenetur dolorem sunt voluptas enim doloremque, voluptatibus obcaecati, dicta alias voluptatum expedita delectus. Illum, consectetur! Lorem ipsum dolor sit amet consectetur adipisicing elit. In ipsum doloribus natus! Consectetur distinctio, ducimus tenetur dolorem sunt voluptas enim',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. In ipsum doloribus natus! Consectetur distinctio, ducimus tenetur dolorem sunt voluptas enim doloremque, voluptatibus obcaecati, dicta alias voluptatum expedita delectus. Illum, consectetur! Lorem ipsum dolor sit amet consectetur adipisicing elit. In ipsum doloribus natus! Consectetur distinctio, ducimus tenetur dolorem sunt voluptas enim doloremque, voluptatibus obcaecati, dicta alias voluptatum expedita delectus. Illum, consectetur! Lorem ipsum dolor sit amet consectetur adipisicing elit. In ipsum doloribus natus! Consectetur distinctio, ducimus tenetur dolorem sunt voluptas enim'
+  ];
+  showHiddenParagraphs: boolean = false;
+
+  toggleHiddenParagraphs() {
+    this.showHiddenParagraphs = !this.showHiddenParagraphs;
+  }
+
+  showForm: boolean = false;
+  assunto: string = '';
+  conteudo: string = '';
+
+  toggleFormVisibility() {
+    this.showForm = !this.showForm;
+    this.clearForm();
+  }
+    clearForm() {
+      this.assunto = '';
+      this.conteudo = '';
+    }
+    submitTopic() {
+      // Lógica para enviar o tópico aqui
+      console.log('Assunto:', this.assunto);
+      console.log('Conteúdo:', this.conteudo);
+      this.toggleFormVisibility();
+    }
+}
+
+
--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterLinkActive } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // Importe o FormsModule
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    [BrowserModule, FormsModule],
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
